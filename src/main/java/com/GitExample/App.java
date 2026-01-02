e1962bdc521706490f33ebf80265a7b0eb22d776
gayathri <gayathrisundaram91@gmail.com>
2026-01-02 11:17:34 +0530

    new example

--- src/main/java/com/GitExample/App.java
+++ src/main/java/com/GitExample/App.java
@@ -1,10 +1,8 @@
 package com.GitExample;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("All is well");
     }
 }
